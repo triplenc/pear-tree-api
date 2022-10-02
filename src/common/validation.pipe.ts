393079cbfe6c5d244e651125cccef26cbab9bcf7
SeongJaeSong <tjdkskgnal61@gmail.com>
2022-10-02 23:08:12 +0900

    validation pipe 구현

---
+++ src/common/validation.pipe.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common'
+import { plainToInstance } from 'class-transformer'
+import { validate } from 'class-validator'
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(
+    value: any,
+    { metatype }: ArgumentMetadata,
+  ): Promise<unknown> {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value
+    }
+    const object = plainToInstance(metatype, value)
+    const errors = await validate(object)
+    if (errors.length > 0) {
+      throw new BadRequestException('Validation failed')
+    }
+    return value
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object]
+    return !types.includes(metatype)
+  }
+}
+
+/* eslint-enable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-enable @typescript-eslint/no-explicit-any */
+/* eslint-enable @typescript-eslint/ban-types */
