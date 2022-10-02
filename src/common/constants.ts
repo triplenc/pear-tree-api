adee93ae21d208d3ae2a27e8479c2b167cbe05af
SeongJaeSong <tjdkskgnal61@gmail.com>
2022-10-02 23:09:17 +0900

    공용 SuccessRO 구현

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,10 @@
+import { SuccessRO } from './common.interface'
+
 export const IMAGE_URL_LENGTH = 255
 
 export const ADDRESS_LENGTH = 100
+
+export const SUCCESS_RO: SuccessRO = {
+  statusCode: 200,
+  message: 'success',
+}
