0eb221980eb50478c461d1f4a6cc16a6be0b93db
SeongJaeSong <tjdkskgnal61@gmail.com>
2022-10-03 14:32:11 +0900

    categories priority 추가

--- src/entities/category/categories.entity.ts
+++ src/entities/category/categories.entity.ts
@@ -3,10 +3,13 @@ import { IMAGE_URL_LENGTH } from '../../common/constants'
 import { ClassificationEntity } from '../classification.entity'
 import { Party } from '../party/parties.entity'
 
-@Entity({ name: 'categories', synchronize: false })
+@Entity({ name: 'categories' })
 export class Category extends ClassificationEntity {
   @Column({ length: IMAGE_URL_LENGTH })
   imageUrl: string
+
+  @Column()
+  priority: number
 
   @OneToMany(() => Party, (party) => party.category)
   partyList: Party[]
